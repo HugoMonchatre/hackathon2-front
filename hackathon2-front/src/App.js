dd95bd0e7c2a652788d778befb5d35b5fb9e7378
FabienNiepceron <fabienniepceron72@gmail.com>
2021-01-14 17:09:43 +0100

    Map à jour geolocalisation OK

--- hackathon2-front/src/App.js
+++ hackathon2-front/src/App.js
@@ -1,11 +1,8 @@
-// import './App.css';
 import Map from './components/Map';
-import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
-      <Title />
       <Map />
     </div>
   );
--- hackathon2-front/src/components/Map.jsx
+++ hackathon2-front/src/components/Map.jsx
@@ -1,18 +1,47 @@
-import React, {useState} from 'react' 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import "./Map.css"
+import React, { useState, useEffect } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from 'react-leaflet';
+import './Map.css';
+import L from 'leaflet';
+import geolocation from 'geolocation';
+
+// const location = ('geolocation')
+
+const markerIcon = new L.icon({
+  iconUrl:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-AhH26-K3HyVfgM0X3g75hrT5PiQzZSAE0Q&usqp=CAU',
+  iconSize: [30, 30],
+});
 
 const Map = () => {
+  const [position, setPosition] = useState(['48.4671488', '1.0125312']);
+
+  useEffect(() => {
+    geolocation.getCurrentPosition(function (err, position) {
+      if (err) {
+        console.log(err);
+      } else {
+        setPosition(position);
+      }
+    });
+  }, []);
+
   return (
-    <MapContainer id="mapid" center={[48.448553368159985, 1.5381089746419778]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer id="mapid" center={position} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[48.448553368159985, 1.5381089746419778]}>
-        <Popup>
-          ComparateurAgricole.com
-        </Popup>
+      <Marker
+        position={[48.448553368159985, 1.5381089746419778]}
+        icon={markerIcon}
+      >
+        <Popup>ComparateurAgricole.com</Popup>
       </Marker>
     </MapContainer>
   );
