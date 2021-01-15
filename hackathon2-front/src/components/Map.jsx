6c41e651ec4e055eb1e6b58bb0de222e0f8b390c
FabienNiepceron <fabienniepceron72@gmail.com>
2021-01-15 09:13:44 +0100

    composant map à jour

--- hackathon2-front/src/components/Map.jsx
+++ hackathon2-front/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLeafletContext } from 'react';
 import {
   MapContainer,
   TileLayer,
@@ -9,40 +9,44 @@ import {
 import './Map.css';
 import L from 'leaflet';
 import geolocation from 'geolocation';
+import LocationMarker from "./leaflet"
 
-// const location = ('geolocation')
 
 const markerIcon = new L.icon({
   iconUrl:
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-AhH26-K3HyVfgM0X3g75hrT5PiQzZSAE0Q&usqp=CAU',
-  iconSize: [30, 30],
+    'https://media-exp1.licdn.com/dms/image/C4D0BAQGYpTU_YfBlyQ/company-logo_200_200/0/1519904413694?e=2159024400&v=beta&t=iGU8ND-fM6mQbBPjhWwBacc5pHCjgVFNQIMi6yy9HA4',
+  iconSize: [(500, 50)],
 });
 
-const Map = () => {
-  const [position, setPosition] = useState(['48.4671488', '1.0125312']);
 
-  useEffect(() => {
-    geolocation.getCurrentPosition(function (err, position) {
-      if (err) {
-        console.log(err);
-      } else {
-        setPosition(position);
-      }
-    });
-  }, []);
+const Map = () => {
+  // const [position, setPosition] = useState(null);
+
+  // useLeafletContext((position) => {
+  //   geolocation.getCurrentPosition(function (err, position) {
+  //     console.log(position)
+  //     if (err) {
+  //       console.log("error");
+  //     } else {
+  //       console.log([position.coords.latitude, position.coords.longitude ]) 
+  //       setPosition  ([position.coords.latitude, position.coords.longitude ]) 
+  //     }
+  //   });
+  // }, []);
 
   return (
-    <MapContainer id="mapid" center={position} zoom={13} scrollWheelZoom={true}>
+    <MapContainer id="mapid" center={[48.448959714197414, 1.537250677752061]} zoom={14} icon={markerIcon} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
       />
       <Marker
-        position={[48.448553368159985, 1.5381089746419778]}
+        position={[48.448959714197414, 1.537250677752061]}
         icon={markerIcon}
       >
-        <Popup>ComparateurAgricole.com</Popup>
+        <Popup><div className="div-popup">ComparateurAgricole.com</div></Popup>
       </Marker>
+      <LocationMarker />
     </MapContainer>
   );
 };
--- hackathon2-front/src/components/SearchButton.jsx
+++ hackathon2-front/src/components/SearchButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./search.css";
-import axios from "axios";
+// import axios from "axios";
 import searchIcon from "../images/magnifier.svg";
 
 const SearchButton = ({ searchBarOn, onClick }) => {
